3aac5b11c1c4eedbd80617bb185b2297fbdde21f
Maxime Raynal <me@maxproject.com>
2014-09-09 21:53:18 +0200

    Ajout de la structure de base de symfony

---
+++ src/app/cache/dev/twig/be/b5/5c5c64b769cbd2f264c79ef2e07c67c89142dddff984e2d8c499ccdc7eb4.php
@@ -0,0 +1,39 @@
+<?php
+
+/* MaxProjectProjectManagmentBundle:Default:index.html.twig */
+class __TwigTemplate_beb55c5c64b769cbd2f264c79ef2e07c67c89142dddff984e2d8c499ccdc7eb4 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "MaxProjectProjectManagmentBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ src/app/cache/dev/twig/eb/e1/78b0a6150be740bbddf989b3a1a07fa70ff3615827c80bdfbfe08503577a.php
@@ -0,0 +1,49 @@
+<?php
+
+/* MaxProjectProjectManagmentBundle:Default:default.html.twig */
+class __TwigTemplate_ebe178b0a6150be740bbddf989b3a1a07fa70ff3615827c80bdfbfe08503577a extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_body($context, array $blocks = array())
+    {
+        // line 3
+        echo "    This is a default view !
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "MaxProjectProjectManagmentBundle:Default:default.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 3,  28 => 2,);
+    }
+}
