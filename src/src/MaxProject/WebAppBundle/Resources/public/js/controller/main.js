58af183b0fa6bbbbece59b9ebf17e1c1c1c7aa13
Maxime Raynal <me@maxproject.fr>
2014-09-24 18:09:30 +0200

    Ajout du module de routing d'angular js

--- src/src/MaxProject/WebAppBundle/Resources/public/js/controller/main.js
+++ src/src/MaxProject/WebAppBundle/Resources/public/js/controller/main.js
@@ -1,3 +1,20 @@
 //Point d'entrée de l'application web
+//Contient la définition de l'app et le rooting
 
-var phonecatApp = angular.module('ProjectManagmentWebApp', []);
+var projectManagmentApp = angular.module('ProjectManagmentWebApp', ['ngRoute', 'ProjectsListCtrl']);
+
+projectManagmentApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/projects', {
+            templateUrl: 'views/projectsList.part.html',
+            controller: 'ProjectsListCtrl'
+        }).
+        when('/projects/:projectId', {
+            templateUrl: 'views/projectDetail.part.html',
+            controller: 'ProjectCtrl'
+        }).
+        otherwise({
+            redirectTo: '/projects'
+        });
+    }]);
