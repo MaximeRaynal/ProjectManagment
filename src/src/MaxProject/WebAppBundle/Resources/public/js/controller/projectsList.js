2ccf61b3664c42f03d1440ae8b860f391308a69a
Maxime Raynal <me@maxproject.fr>
2014-09-24 18:11:45 +0200

    Utilisation des vues et du routing d'angular js

--- src/src/MaxProject/WebAppBundle/Resources/public/js/controller/projectsList.js
+++ src/src/MaxProject/WebAppBundle/Resources/public/js/controller/projectsList.js
@@ -1,4 +1,4 @@
-phonecatApp.controller('ProjectsListCtrl', function ($scope, $http) {
+projectManagmentApp.controller('ProjectsListCtrl', function ($scope, $http) {
     $http.get('api/projects.json').success(function(data) {
         $scope.projects = data;
     });
